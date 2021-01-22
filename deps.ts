59b82c978df7ddfafe960e123cd49ec220d79c22
Robin Bertram <robin.bertram@gmail.com>
2021-01-22 21:31:31 +0100

    Bumped std version to 0.84

--- deps.ts
+++ deps.ts
@@ -1,8 +1,5 @@
-export {
-  serve,
-  ServerRequest,
-} from "https://deno.land/std@0.83.0/http/server.ts";
-export * as Webserver from "https://deno.land/std@0.83.0/ws/mod.ts";
-export type { WebSocket } from "https://deno.land/std@0.83.0/ws/mod.ts";
-export { Client as MysqlClient } from "https://deno.land/x/mysql/mod.ts";
-export { connect as RedisConnect } from "https://deno.land/x/redis/mod.ts";
+export { serve, ServerRequest } from 'https://deno.land/std@0.84.0/http/server.ts';
+export * as Webserver from 'https://deno.land/std@0.84.0/ws/mod.ts';
+export type { WebSocket } from 'https://deno.land/std@0.84.0/ws/mod.ts';
+export { Client as MysqlClient } from 'https://deno.land/x/mysql/mod.ts';
+export { connect as RedisConnect } from 'https://deno.land/x/redis/mod.ts';
