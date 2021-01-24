e068f1d3d71ee133aff71c2ed707fe9d75bffa36
Robin Bertram <robin.bertram@gmail.com>
2021-01-24 17:17:09 +0100

    Refactorings

--- src/Registry/Registry.ts
+++ src/Registry/Registry.ts
@@ -1,30 +1,39 @@
 export class Registry {
-    public static instance: Registry;
+    public static instance: Registry | null;
     public static instances: any = {};
 
     constructor() {
         if (Registry.instance) {
-            throw new Error('Config is already initiated');
+            throw new Error('Registry is already initiated');
         }
 
         Registry.instance = this;
     }
 
-    public registerMany(instances: Array<any>) {
+    public registerMany(instances: Array<any>): this {
         for (const instance of instances) {
             this.register(instance);
         }
+
+        return this;
     }
 
-    public register(instance: any) {
+    public register(instance: any): this {
         const name = instance.constructor.name;
 
         if (!Registry.instances[name]) {
             Registry.instances[name] = instance;
         }
+
+        return this;
     }
 
-    public static get(instance: any) {
+    public static get(instance: any): any {
         return this.instances[instance.name];
+    }
+
+    public static clear(): void {
+        this.instance = null;
+        this.instances = {};
     }
 }
